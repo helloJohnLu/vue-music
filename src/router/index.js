62ed5ac3602cd413089ae20152ecf91cf487d3a8
John Lu <johnluchn@gmail.com>
2020-03-17 01:25:38 +0800

    94 路由性能优化：组件按需加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Recommend from "../views/Recommend";
-import Rank from "../views/Rank";
-import Search from "../views/Search";
-import Singer from "../views/Singer";
+// 注意点：通过 import xxx from xxx 的方式加载组件，无论组件是否被使用，都会被加载
+// import Recommend from "../views/Recommend";
+// import Rank from "../views/Rank";
+// import Search from "../views/Search";
+// import Singer from "../views/Singer";
+
+// 路由性能优化：组件按需加载
+const Recommend = resolve => {
+  import('../views/Recommend').then(module => resolve(module));
+}
+const Rank = resolve => {
+  import('../views/Rank').then(module => resolve(module));
+}
+const Singer = resolve => {
+  import('../views/Singer').then(module => resolve(module));
+}
+const Search = resolve => {
+  import('../views/Search').then(module => resolve(module));
+}
 
 Vue.use(VueRouter)
 
