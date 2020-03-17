4dbd7a5a8762cea2f662d018cdb9f16d3669832a
John Lu <johnluchn@gmail.com>
2020-03-17 15:47:11 +0800

    99 最新音乐

--- src/views/Recommend.vue
+++ src/views/Recommend.vue
@@ -3,26 +3,29 @@
     <Banner :banners="banners"></Banner>
     <Personalized :personalized="personalized" :title="'推荐歌单'"></Personalized>
     <Personalized :personalized="albums" :title="'最新专辑'"></Personalized>
+    <SongList :songs="songs"></SongList>
   </div>
 </template>
 
 <script>
-  import {getBanner, getPersonalized, getNewAlbum} from "../api/index";
+  import {getBanner, getPersonalized, getNewAlbum, getNewSong} from "../api/index";
   import Banner from "../components/Banner";
   import Personalized from "../components/Personalized";
+  import SongList from "../components/SongList";
 
   export default {
     name: "Recommend",
     components: {
       Banner,
       Personalized,
-      getNewAlbum
+      SongList
     },
     data() {
       return {
         banners: [],
         personalized: [],
-        albums: []
+        albums: [],
+        songs: []
       }
     },
     created() {
@@ -34,6 +37,9 @@
         .catch(err => console.log(err));
       getNewAlbum()
         .then(data => this.albums = data.albums.splice(0, 6))
+        .catch(err => console.log(err));
+      getNewSong()
+        .then(data => this.songs = data.result)
         .catch(err => console.log(err));
     }
   }
