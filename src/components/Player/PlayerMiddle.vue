9e4f7a8521bdd9a6cd623bc80ca2ebf0898b9ce2
John Lu <johnluchn@gmail.com>
2020-03-18 23:37:33 +0800

    112-3 播放器界面中间部分，歌词样式

--- src/components/Player/PlayerMiddle.vue
+++ src/components/Player/PlayerMiddle.vue
@@ -8,7 +8,60 @@
       <p>歌曲名歌曲名歌曲名歌曲名</p>
     </swiper-slide>
     <swiper-slide class="lyric">
-
+      <ScrollView>
+        <ul>
+          <li>我是第1个li</li>
+          <li>我是第2个li</li>
+          <li>我是第3个li</li>
+          <li>我是第4个li</li>
+          <li>我是第5个li</li>
+          <li>我是第6个li</li>
+          <li>我是第7个li</li>
+          <li>我是第8个li</li>
+          <li>我是第9个li</li>
+          <li>我是第10个li</li>
+          <li>我是第11个li</li>
+          <li>我是第12个li</li>
+          <li>我是第13个li</li>
+          <li>我是第14个li</li>
+          <li>我是第15个li</li>
+          <li>我是第16个li</li>
+          <li>我是第17个li</li>
+          <li>我是第18个li</li>
+          <li>我是第19个li</li>
+          <li>我是第20个li</li>
+          <li>我是第21个li</li>
+          <li>我是第22个li</li>
+          <li>我是第23个li</li>
+          <li>我是第24个li</li>
+          <li>我是第25个li</li>
+          <li>我是第26个li</li>
+          <li>我是第27个li</li>
+          <li>我是第28个li</li>
+          <li>我是第29个li</li>
+          <li>我是第30个li</li>
+          <li>我是第31个li</li>
+          <li>我是第32个li</li>
+          <li>我是第33个li</li>
+          <li>我是第34个li</li>
+          <li>我是第35个li</li>
+          <li>我是第36个li</li>
+          <li>我是第37个li</li>
+          <li>我是第38个li</li>
+          <li>我是第39个li</li>
+          <li>我是第40个li</li>
+          <li>我是第41个li</li>
+          <li>我是第42个li</li>
+          <li>我是第43个li</li>
+          <li>我是第44个li</li>
+          <li>我是第45个li</li>
+          <li>我是第46个li</li>
+          <li>我是第47个li</li>
+          <li>我是第48个li</li>
+          <li>我是第49个li</li>
+          <li>我是第50个li</li>
+        </ul>
+      </ScrollView>
     </swiper-slide>
     <!-- Optional controls -->
     <div class="swiper-pagination" slot="pagination"></div>
@@ -18,12 +71,14 @@
 <script>
   import 'swiper/dist/css/swiper.css'
   import {swiper, swiperSlide} from 'vue-awesome-swiper'
+  import ScrollView from "../ScrollView";
 
   export default {
     name: "PlayerMiddle",
     components: {
       swiper,
-      swiperSlide
+      swiperSlide,
+      ScrollView
     },
     data() {
       return {
@@ -76,6 +131,19 @@
         text-align: center;
         @include font_color();
         @include font_size($font_medium);
+      }
+    }
+
+    .lyric {
+      li {
+        text-align: center;
+        @include font_size($font_medium_s);
+        @include font_color();
+        margin: 10px 0;
+
+        &:last-of-type {
+          padding-bottom: 100px;
+        }
       }
     }
   }
