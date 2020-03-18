9aeea3facd8342ed749b63de78c81f8b2115c31c
John Lu <johnluchn@gmail.com>
2020-03-19 00:29:03 +0800

    113-2 播放器界面底部，button

--- src/components/Player/PlayerBottom.vue
+++ src/components/Player/PlayerBottom.vue
@@ -9,7 +9,13 @@
       </div>
       <span>0:00</span>
     </div>
-    <div class="bottom-controll"></div>
+    <div class="bottom-control">
+      <div class="mode"></div>
+      <div class="prev"></div>
+      <div class="play"></div>
+      <div class="next"></div>
+      <div class="favorite"></div>
+    </div>
   </div>
 </template>
 
@@ -20,6 +26,9 @@
 </script>
 
 <style scoped lang="scss">
+  @import "../../assets/css/variable";
+  @import "../../assets/css/mixin";
+
   .player-bottom {
     position: fixed;
     left: 0;
@@ -32,6 +41,11 @@
       display: flex;
       justify-content: space-between;
       align-items: center;
+
+      span {
+        @include font_size($font_small);
+        @include font_color();
+      }
 
       .progress-bar {
         width: 100%;
@@ -55,6 +69,43 @@
             top: 50%;
             transform: translateY(-50%);
           }
+        }
+      }
+    }
+
+    .bottom-control {
+      width: 80%;
+      margin: 0 auto;
+      padding: 50px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      div {
+        width: 84px;
+        height: 84px;
+      }
+
+      .mode {
+        @include bg_img('../../assets/images/loop');
+      }
+
+      .prev {
+        @include bg_img('../../assets/images/prev');
+      }
+
+      .play {
+        @include bg_img('../../assets/images/pause');
+      }
+
+      .next {
+        @include bg_img('../../assets/images/next');
+      }
+
+      .favorite {
+        @include bg_img('../../assets/images/un_favorite');
+        &.active {
+          @include bg_img('../../assets/images/favorite');
         }
       }
     }
