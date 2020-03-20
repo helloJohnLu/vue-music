88c493924c6da80008252b8b6cf8358cbc52d6b0
John Lu <johnluchn@gmail.com>
2020-03-20 10:44:25 +0800

    119-3 Vuex 优化，mutations 使用变量代替字符串，防止易错

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,15 @@
+import {
+  SET_FULL_SCREEN
+} from "./mutations-type";
+
 export default {
+  /*
   setFullScreen({commit}, flag) {
-    commit('changeFullScreen', flag);
+    commit('SET_FULL_SCREEN', flag);
+  }
+   */
+
+  setFullScreen({commit}, flag) {
+    commit(SET_FULL_SCREEN, flag);
   }
 }
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,5 +1,16 @@
+import {
+  SET_FULL_SCREEN
+} from "./mutations-type";
+
+
 export default {
+  /*
   changeFullScreen(state, flag) {
+    state.isFullScreen = flag;
+  }
+   */
+
+  [SET_FULL_SCREEN](state, flag) {
     state.isFullScreen = flag;
   }
 }
