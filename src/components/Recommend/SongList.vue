b2d0cdd7940fb5c9cc62afc03d787f62941506de
John Lu <johnluchn@gmail.com>
2020-03-20 10:50:14 +0800

    120-1 最新音乐点击跳转到默认播放界面

--- src/components/Recommend/SongList.vue
+++ src/components/Recommend/SongList.vue
@@ -4,7 +4,7 @@
       <h3>最新音乐</h3>
     </div>
     <ul class="song-list">
-      <li v-for="value in songs" :key="value.id" class="item">
+      <li v-for="value in songs" :key="value.id" class="item" @click="selectMusic">
 <!--        <img :src="value.song.album.picUrl" alt="">-->
         <img v-lazy="value.song.album.picUrl" alt="">
         <div>
@@ -17,6 +17,8 @@
 </template>
 
 <script>
+  import {mapActions} from "vuex";
+
   export default {
     name: "SongList",
     props: {
@@ -24,6 +26,14 @@
         type: Array,
         default: () => [],
         required: true
+      }
+    },
+    methods: {
+      ...mapActions([
+        'setFullScreen'
+      ]),
+      selectMusic() {
+        this.setFullScreen(true);
       }
     }
   }
