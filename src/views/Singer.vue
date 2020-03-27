297316b14c5a9da00ec32ee5e54a173d687691b8
John Lu <johnluchn@gmail.com>
2020-03-27 12:19:54 +0800

    158-2 优化：滚动界面，对应的导航字母高亮 —— 节流

--- src/views/Singer.vue
+++ src/views/Singer.vue
@@ -110,7 +110,9 @@
     },
     mounted() {
       // 监听滚动
-      this.$refs.scrollView.scrolling(y => {
+      let timer = setTimeout(this.$refs.scrollView.scrolling(y => {
+        clearTimeout(timer);
+
         // 处理第一个 item ：向下滚动，始终是第一组
         if (y >= 0) {
           return this.currentIndex = 0;
@@ -121,13 +123,12 @@
           let nextTop = this.groupsItemTop[i + 1];
           // 如果滚上去的距离处于两个元素距离之间
           if (-y >= preTop && -y < nextTop) {
-            this.currentIndex = i;
-            return ;
+            return this.currentIndex = i;
           }
         }
         // 最后一个 item
-        this.currentIndex = this.groupsItemTop.length - 1;
-      })
+        return this.currentIndex = this.groupsItemTop.length - 1;
+      }), 100);
     },
     components: {
       ScrollView
