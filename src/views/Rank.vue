882d4475932b6ff8580287d8864d6656c82cba91
John Lu <johnluchn@gmail.com>
2020-03-28 09:11:35 +0800

    163-1 排行榜数据填充 —— 标题

--- src/views/Rank.vue
+++ src/views/Rank.vue
@@ -1,23 +1,57 @@
 <template>
-  <div class="rand">
-    排行
+  <div class="rank">
+    <ScrollView>
+      <ul>
+        <li v-for="(value, key) in category.titles" :key="key">
+          <h3 class="group-title">{{value}}</h3>
+        </li>
+      </ul>
+    </ScrollView>
   </div>
 </template>
 
 <script>
   import {getTopListDetail} from "../api/index";
-
+  import ScrollView from "../components/ScrollView";
 
   export default {
     name: "Rank",
+    components: {
+      ScrollView
+    },
+    data: function () {
+      return {
+        category: {}
+      }
+    },
     created() {
       getTopListDetail()
-        .then(data => console.log(data))
+        .then(data => {
+          console.log(data);
+          this.category = data;
+        })
         .catch(error => console.log(error));
     }
   }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  @import "../assets/css/variable";
+  @import "../assets/css/mixin";
 
+  .rank {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 184px;
+    bottom: 0;
+    @include bg_sub_color();
+
+    .group-title {
+      padding: 10px 20px;
+      @include font_color();
+      @include font_size($font_large);
+      font-weight: bold;
+    }
+  }
 </style>
